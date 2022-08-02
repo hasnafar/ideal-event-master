b9ecebdf4725e4337ec4bdfa6c1733556bbd37ca
hasnafar <hasnafarrukh@gmail.com>
2022-08-02 15:48:48 -0400

    Get data from API

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,94 @@
-var DATA;
-const mapboxKey="pk.eyJ1IjoiYWlqYXptIiwiYSI6ImNra2ZwcDdvZTA2ZHoydW15NjFjcTF6cmwifQ.XLBRPPbiyXBl4BIAgUr0Qw";
-var url="https://app.ticketmaster.com/discovery/v2/events.json?radius=30&sort=date,asc&apikey=pLOeuGq2JL05uEGrZG7DuGWu6sh2OnMz";
+
+//Declaring Global Variables
+
+var ZIP_CITY="";
+var CATEGORY="";
+var DATA; //helper to debug from console
+
+var LONG;
+var LAT;
+
+//Getting data from user
 
 
-mapboxgl.accessToken=mapboxKey;
+$(".search").on("click",function(){
+    ZIP_CITY=$("#zip").val();
+    $("#list-results").empty();   // Clear the list every time you click search so it can be repopulated
+    displayData();
 
-
-        var map=new mapboxgl.Map({
-                container: "map", //selects div id
-                style: "mapbox://styles/mapbox/light-v10", //from website
-                center:[-74.484247,40.450363], //user location
-                zoom: 9 //starting zoom
-        });
-
-
-        $.get(url,function(data){
-            console.log(data);
-            DATA=data;
 })
 
+//Displaying data
 
+function displayData(){
+
+    //Getting Latitude and Longitude Data From Mapbox API
+
+    const mapboxKey="pk.eyJ1IjoiYWlqYXptIiwiYSI6ImNra2ZwcDdvZTA2ZHoydW15NjFjcTF6cmwifQ.XLBRPPbiyXBl4BIAgUr0Qw";
+
+    var zipURL="https://api.mapbox.com/geocoding/v5/mapbox.places/"+ZIP_CITY+".json?access_token=pk.eyJ1IjoiYWlqYXptIiwiYSI6ImNra2ZwcDdvZTA2ZHoydW15NjFjcTF6cmwifQ.XLBRPPbiyXBl4BIAgUr0Qw";
+
+
+    $.ajax({
+        async: false, //wait till data is received
+        type: 'GET',
+        url: zipURL,
+        success: function(data) {
+        LONG=data.features[0].center[0];
+        LAT=data.features[0].center[1];
+        }
+    });
+
+
+    //Creating new map
+
+    mapboxgl.accessToken=mapboxKey;
+
+
+    var map=new mapboxgl.Map({
+            container: "map", //selects div id
+            style: "mapbox://styles/mapbox/light-v10", //from website
+            center:[LONG,LAT], //user location
+            zoom: 7 //starting zoom
+    });
+    
+    
+
+    //Creates url for events within 30 mile radius of long and lat given + category football + sort by ascending dates
+
+    var url="https://app.ticketmaster.com/discovery/v2/events.json?radius=30&classificationName="+CATEGORY+"&sort=date,asc&apikey=pLOeuGq2JL05uEGrZG7DuGWu6sh2OnMz&latlong="+LAT+","+LONG;
+
+    $.get(url,function(data){
+        console.log(data);
+        DATA=data;
+
+        var numberOfEvents=data._embedded.events.length;
+
+        //Getting Data and Formatting
+        for (var i=0;i<numberOfEvents;i++) {
+
+        var name=data._embedded.events[i].name;
+
+        var date=data._embedded.events[i].dates.start.localDate;
+        date=moment(date).format('dddd,  MMMM Do');
+
+        var startTime=data._embedded.events[i].dates.start.localTime;
+        startTime=moment(startTime,"HH:mm:ss").format('LT');
+
+        var cityName=data._embedded.events[i]._embedded.venues[0].city.name;
+
+        var ticketUrl=data._embedded.events[i].url;
+
+        var venueLongitude=data._embedded.events[i]._embedded.venues[0].location.longitude;
+        var venueLatitude=data._embedded.events[i]._embedded.venues[0].location.latitude;
+
+    
+        //Displaying Data
+ 
+        
+        
+        }
+        
+    })
+
+}
